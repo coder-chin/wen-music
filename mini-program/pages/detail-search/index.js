a75d269d51c0104b12bf158d383336740be7cd76
chin <1277180540@qq.com>
2022-04-15 10:37:04 +0800

    feat: search-page

--- mini-program/pages/detail-search/index.js
+++ mini-program/pages/detail-search/index.js
@@ -1,66 +1,70 @@
 // pages/detail-search/index.js
+import { getSearchHot, getSearchSuggest, getSearchResult } from '../../api/search'
+import debounce from '../../utils/debounce'
+import stringToNodes from '../../utils/string2nodes'
+
+const debounceGetSearchSuggest = debounce(getSearchSuggest, 300)
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    hotKeywords: [],
+    suggestSongs: [],
+    suggestSongsNodes: [],
+    resultSongs: [],
+    searchValue: ""
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad: function () {
+    this.getPageData()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getPageData: async function() {
+    const res = await getSearchHot()
+    this.setData({
+      hotKeywords: res.result.hots
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  // 输入内容改变回调
+  handleSearchChange: function(event) {
+    const searchValue = event.detail
+    this.setData({ searchValue })
 
+    if (!searchValue.length) {
+      this.setData({ suggestSongs: [], resultSongs: [] })
+      debounceGetSearchSuggest.cancel()
+      return
+    }
+
+    debounceGetSearchSuggest(searchValue).then(res => {
+      // 1.获取建议的关键字歌曲
+      const suggestSongs = res.result.allMatch
+      this.setData({ suggestSongs })
+      if (!suggestSongs) return
+
+      // 2.转成nodes节点
+      const suggestKeywords = suggestSongs.map(item => item.keyword)
+      const suggestSongsNodes = []
+      for (const keyword of suggestKeywords) {
+        const nodes = stringToNodes(keyword, searchValue)
+        suggestSongsNodes.push(nodes)
+      }
+      this.setData({ suggestSongsNodes })
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  // 点击搜索回调
+  handleSearchAction: function() {
+    const searchValue = this.data.searchValue
+    getSearchResult(searchValue).then(res => {
+      this.setData({ resultSongs: res.result.songs })
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 点击推荐热词
+  handleKeywordItemClick: function(event) {
+    const keyword = event.currentTarget.dataset.keyword
+    this.setData({ searchValue: keyword })
+    this.handleSearchAction()
   }
 })
