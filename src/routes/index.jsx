f0928a58b19a4f2a885bbda1de797370f89fec3b
chin <1277180540@qq.com>
2022-09-24 14:36:44 +0800

    perf:  implement lazy loading

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,18 +2,12 @@ import { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from '@/pages/Home'
-import Recommend from '@/pages/Recommend'
-import Singers from '@/pages/Singers'
-import Rank from '@/pages/Rank'
-import Album from '@/pages/Album'
-import Singer from '@/pages/Singer'
-import Search from '@/pages/Search'
-// const Recommend = lazy(() => import('@/pages/Recommend'))
-// const Singers = lazy(() => import('@/pages/Singers'))
-// const Rank = lazy(() => import('@/pages/Rank'))
-// const Album = lazy(() => import('@/pages/Album'))
-// const Singer = lazy(() => import('@/pages/Singer'))
-// const Search = lazy(() => import('@/pages/Search'))
+const Recommend = lazy(() => import('@/pages/Recommend'))
+const Singers = lazy(() => import('@/pages/Singers'))
+const Rank = lazy(() => import('@/pages/Rank'))
+const Album = lazy(() => import('@/pages/Album'))
+const Singer = lazy(() => import('@/pages/Singer'))
+const Search = lazy(() => import('@/pages/Search'))
 
 const SuspenseComponent = (Component) => (props) => {
   return (
@@ -28,17 +22,25 @@ export default function AppRouter() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}>
-          <Route index element={<Recommend />}></Route>
-          <Route path="singers" element={<Singers />}>
-            <Route path=":id" element={SuspenseComponent(Singer)} />
+          <Route index element={SuspenseComponent(Recommend)()}></Route>
+
+          <Route path="recommend">
+            <Route path=":id" element={SuspenseComponent(Album)()} />
           </Route>
-          <Route path="rank" element={<Rank />}>
-            <Route path=":id" element={SuspenseComponent(Album)} />
+
+          <Route path="singers" element={SuspenseComponent(Singers)()}>
+            <Route path=":id" element={SuspenseComponent(Singer)()} />
           </Route>
-          <Route path="album" element={<Album />}>
-            <Route path=":id" element={SuspenseComponent(Album)} />
+
+          <Route path="rank" element={SuspenseComponent(Rank)()}>
+            <Route path=":id" element={SuspenseComponent(Album)()} />
           </Route>
-          <Route path="search" element={<Search />}></Route>
+
+          <Route path="album">
+            <Route path=":id" element={SuspenseComponent(Album)()} />
+          </Route>
+
+          <Route path="search" element={SuspenseComponent(Search)()}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
