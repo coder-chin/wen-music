0976ea81b64ee9b0ca597cc4a038fcd70b7d91fe
chin <1277180540@qq.com>
2022-09-24 14:05:24 +0800

    chore: switch to react-touter v6

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,19 @@
 import { lazy, Suspense } from 'react'
-import { Redirect } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from '@/pages/Home'
-const Recommend = lazy(() => import('@/pages/Recommend'))
-const Singers = lazy(() => import('@/pages/Singers'))
-const Rank = lazy(() => import('@/pages/Rank'))
-const Album = lazy(() => import('@/pages/Album'))
-const Singer = lazy(() => import('@/pages/Singer'))
-const Search = lazy(() => import('@/pages/Search'))
+import Recommend from '@/pages/Recommend'
+import Singers from '@/pages/Singers'
+import Rank from '@/pages/Rank'
+import Album from '@/pages/Album'
+import Singer from '@/pages/Singer'
+import Search from '@/pages/Search'
+// const Recommend = lazy(() => import('@/pages/Recommend'))
+// const Singers = lazy(() => import('@/pages/Singers'))
+// const Rank = lazy(() => import('@/pages/Rank'))
+// const Album = lazy(() => import('@/pages/Album'))
+// const Singer = lazy(() => import('@/pages/Singer'))
+// const Search = lazy(() => import('@/pages/Search'))
 
 const SuspenseComponent = (Component) => (props) => {
   return (
@@ -17,47 +23,24 @@ const SuspenseComponent = (Component) => (props) => {
   )
 }
 
-export default [
-  {
-    path: '/',
-    component: Home,
-    routes: [
-      {
-        path: '/',
-        component: SuspenseComponent(Recommend)
-      },
-      {
-        path: '/recommend/:id',
-        component: SuspenseComponent(Album)
-      },
-      {
-        path: '/singers',
-        component: SuspenseComponent(Singers),
-        routes: [
-          {
-            path: '/singers/:id',
-            component: SuspenseComponent(Singer)
-          }
-        ]
-      },
-      {
-        path: '/rank',
-        component: SuspenseComponent(Rank),
-        routes: [
-          {
-            path: '/rank/:id',
-            component: SuspenseComponent(Album)
-          }
-        ]
-      },
-      {
-        path: '/album/:id',
-        component: SuspenseComponent(Album)
-      },
-      {
-        path: '/search',
-        component: SuspenseComponent(Search)
-      }
-    ]
-  }
-]
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Recommend />}></Route>
+          <Route path="singers" element={<Singers />}>
+            <Route path=":id" element={SuspenseComponent(Singer)} />
+          </Route>
+          <Route path="rank" element={<Rank />}>
+            <Route path=":id" element={SuspenseComponent(Album)} />
+          </Route>
+          <Route path="album" element={<Album />}>
+            <Route path=":id" element={SuspenseComponent(Album)} />
+          </Route>
+          <Route path="search" element={<Search />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
